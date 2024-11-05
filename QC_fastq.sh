146adb85626d75f53d2c6eecc0209accb17db838
aboss <anna.boss@strudents.unibe.ch>
2024-11-05 21:22:01 +0100

    multiple components copy/paste

--- QC_fastq.sh
+++ QC_fastq.sh
@@ -1,30 +1,46 @@
-#!/bin bash
+#!/bin/bash
 
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1000M
+#SBATCH --mem=1000MG
 #SBATCH --time=01:00:00
 #SBATCH --partition=pibu_el8
 #SBATCH --job-name=fastq
 #SBATCH --mail-user=anna.boss@students.unibe.ch
-#SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/aboss/slurm_tutorial/output_alignment_%A_%a.o
-#SBATCH --error=/data/users/aboss/slurm_tutorial/error_alignment_%A_%a.e
+#SBATCH --mail-type=fail
+#SBATCH --error=/data/users/aboss/rna_course/error_fastq_%A_%a.e
 
+#create variables and links 
 READS_DIR=/data/courses/rnaseq_course/breastcancer_de
-FASTQ_OUTPUT=/data/users/aboss/slurm_tutorial/ecoli/alignment
+FASTQ_OUTPUT=/data/users/aboss/rna_course
+group='HER2'
 
-mkdir -p $ALIGN_DIR
+mkdir /data/users/aboss/fastq_output
 
-cd $READS_DIR
+
+#reads
+ln -s /data/courses/rnaseq_course/breastcancer_de/reads/${group}1_R1.fastq.gz ${group}1_R1.fastq.gz
+ln -s /data/courses/rnaseq_course/breastcancer_de/reads/${group}1_R2.fastq.gz ${group}1_R2.fastq.gz
+
+for k in `ls -1 ${group}*.fastq.gz`;
+do fastqc -t 2 ${k};
+done
+
 
 FILES=(*.fastq)
 
 apptainer exec \
 --bind /data/courses \
-/data/courses/HPC_tutorial/containers/minimap2.sif \
-minimap2 \
+/containers/apptainer/fastqc-0.12.1.siff \
+fastq \
 -a \
 -x sr \
 $REFERENCE_DIR/ecoli-strK12-MG1655.fasta \
 ${FILES[$SLURM_ARRAY_TASK_ID]} \
 > $ALIGN_DIR/${FILES[$SLURM_ARRAY_TASK_ID]}.sam 
+
+
+
+
+
+
+                          
