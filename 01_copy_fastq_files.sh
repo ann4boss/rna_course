91cef7d4d931a18168c02712ae20b9865c8b907e
aboss <anna.boss@strudents.unibe.ch>
2024-11-12 10:05:35 +0100

    small change

--- 01_copy_fastq_files.sh
+++ 01_copy_fastq_files.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1000
-#SBATCH --time=01:00:00
+#SBATCH --time=0:30:00
 #SBATCH --partition=pibu_el8
 #SBATCH --job-name=linkfiles
 #SBATCH --mail-user=anna.boss@students.unibe.ch
@@ -14,9 +14,6 @@ SOURCE_DIR="/data/courses/rnaseq_course/breastcancer_de/reads"
 # Define the directory containing the soft link to RNA-seq reads
 READS_DIR="/data/users/aboss/rna_course/reads"
 mkdir -p ${READS_DIR}
-# Directory for FastQC output    
-OUTPUT_DIR="/data/users/aboss/rna_course/fastqc_results"
-mkdir -p ${OUTPUT_DIR}
 # strains
 STRAINS=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
 
