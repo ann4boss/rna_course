8b1786625fca65fc0b46a3dbb6a7519d250a4ac2
aboss <anna.boss@strudents.unibe.ch>
2024-11-14 11:44:41 +0100

    change strain to sample

--- 01_link_fastq_files.sh
+++ 01_link_fastq_files.sh
@@ -14,15 +14,15 @@ SOURCE_DIR="/data/courses/rnaseq_course/breastcancer_de/reads"
 # Define the directory containing the soft link to RNA-seq reads
 READS_DIR="/data/users/aboss/rna_course/reads"
 mkdir -p ${READS_DIR}
-# strains
-STRAINS=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
+# samples
+SAMPLES=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
 
 # link to reads
-# Loop through each strain
-for STRAIN in "${STRAINS[@]}"; do
+# Loop through each sample
+for SAMPLE in "${SAMPLES[@]}"; do
     # Define the R1 and R2 file paths
-    R1_FILE="${STRAIN}_R1.fastq.gz"
-    R2_FILE="${STRAIN}_R2.fastq.gz"
+    R1_FILE="${SAMPLE}_R1.fastq.gz"
+    R2_FILE="${SAMPLE}_R2.fastq.gz"
     # Create the soft link for the R1 file
     ln -s ${SOURCE_DIR}/${R1_FILE} ${READS_DIR}/${R1_FILE}
     # Create the soft link for the R2 file
--- 02_QC_fastq.sh
+++ 02_QC_fastq.sh
@@ -16,35 +16,35 @@ mkdir -p ${READS_DIR}
 # Directory for FastQC output    
 OUTPUT_DIR="/data/users/aboss/rna_course/fastqc_results"
 mkdir -p ${OUTPUT_DIR}
-# strains
-STRAINS=("Normal3" "TNBC1" "TNBC2" "TNBC3")
-#STRAINS=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
+# samples
+SAMPLES=("Normal3" "TNBC1" "TNBC2" "TNBC3")
+#SAMPLES=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
 # apptainer paths
 APPTAINER_fastqc="/containers/apptainer/fastqc-0.12.1.sif"
 APPTAINER_multiqc="/containers/apptainer/multiqc-1.19.sif"
 
-# Loop through all strains
-for STRAIN in "${STRAINS[@]}"; do
-    # Define the R1 and R2 read files based on the strain
-    READ1="${READS_DIR}/${STRAIN}_R1.fastq.gz"
-    READ2="${READS_DIR}/${STRAIN}_R2.fastq.gz"
+# Loop through all SAMPLES
+for SAMPLE in "${SAMPLES[@]}"; do
+    # Define the R1 and R2 read files based on the sample
+    READ1="${READS_DIR}/${SAMPLE}_R1.fastq.gz"
+    READ2="${READS_DIR}/${SAMPLE}_R2.fastq.gz"
     
     # Check if both READ1 and READ2 files exist
     if [[ -e "${READ1}" && -e "${READ2}" ]]; then
-        echo "Running FastQC for strain: ${STRAIN}"
+        echo "Running FastQC for SAMPLE: ${SAMPLE}"
         
         # Run FastQC using Apptainer
         apptainer exec ${APPTAINER_fastqc} fastqc -o ${OUTPUT_DIR} ${READ1} ${READ2}
         
         # Check if FastQC was successful
         if [[ $? -eq 0 ]]; then
-            echo "FastQC completed for ${STRAIN}."
+            echo "FastQC completed for ${SAMPLE}."
         else
-            echo "Error running FastQC for ${STRAIN}."
+            echo "Error running FastQC for ${SAMPLE}."
         fi
     else
         # If either READ1 or READ2 file is missing, print a warning
-        echo "Missing files for strain: ${STRAIN}. Skipping FastQC."
+        echo "Missing files for SAMPLE: ${SAMPLE}. Skipping FastQC."
     fi
 done
 
