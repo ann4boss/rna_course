ce5a76d92d49724951d9931b5453aee9f4a2e58d
aboss <anna.boss@strudents.unibe.ch>
2024-11-19 19:45:54 +0100

    change of file names

--- 07_count_reads_per_gene.sh
+++ 07_count_reads_per_gene.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4000
-#SBATCH --time=13:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=pibu_el8
 #SBATCH --job-name=count_read_per_gene
 #SBATCH --mail-user=anna.boss@students.unibe.ch
@@ -12,11 +12,11 @@
 # Working directory
 WORK_DIR=/data/users/aboss/rna_course/
 # Directory containing the processed BAM files
-BAM_DIR=/data/users/aboss/rna_course/mapping_results/processed_bam_files
+BAM_DIR=/data/users/aboss/rna_course/06_mapping_results
 # Annotation file path (e.g., GTF file)
-ANNOTATION_FILE=/data/users/aboss/rna_course/reference_genome/Homo_sapiens.GRCh38.113.gtf
+ANNOTATION_FILE=/data/users/aboss/rna_course/04_reference_genome/Homo_sapiens.GRCh38.113.gtf
 # Output directory and file
-OUTPUT_DIR=/data/users/aboss/rna_course/featureCounts_results
+OUTPUT_DIR=/data/users/aboss/rna_course/07_featureCounts_results
 mkdir -p ${OUTPUT_DIR}
 OUTPUT_FILE=${OUTPUT_DIR}/gene_counts.txt
 # Apptainer paths
