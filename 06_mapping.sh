08be2c79e6a8b25eec62318962067ce8cd812ddd
aboss <anna.boss@strudents.unibe.ch>
2024-11-27 16:25:18 +0100

    input of mapping stats output

--- 06_mapping.sh
+++ 06_mapping.sh
@@ -13,9 +13,10 @@
 
 
 # Directory with input files fastq from cleaned files
+#READS_DIR=/data/users/aboss/rna_course/03_cleaning_results
 READS_DIR=/data/users/aboss/rna_course/03_cleaning_results
 # Directory for mapping output    
-OUTPUT_DIR=/data/users/aboss/rna_course/06_mapping_results
+OUTPUT_DIR=/data/users/aboss/rna_course/06_mapping_results_stranded
 mkdir -p ${OUTPUT_DIR}
 # Directory with reference genome and indexed files
 REFERENCE_GENOME_DIR=/data/users/aboss/rna_course/04_reference_genome
@@ -38,10 +39,17 @@ BAM_OUTPUT="${OUTPUT_DIR}/${SAMPLE}_aligned.sorted.bam"
     
 # Run HISAT2 (for aligning) and samtools (for converting to BAM, sorting, and indexing) using Apptainer
 apptainer exec --bind ${REFERENCE_GENOME_DIR} --bind ${READS_DIR} --bind ${OUTPUT_DIR} ${APPTAINER} bash -c "
-hisat2 -x ${HISAT2_INDEX} -1 ${READ1} -2 ${READ2} | \
+hisat2 --rna-strandness FR -x ${HISAT2_INDEX} -1 ${READ1} -2 ${READ2} | \
 samtools view -b - | \
 samtools sort -o ${BAM_OUTPUT} - && \
-samtools index ${BAM_OUTPUT}
+samtools index ${BAM_OUTPUT} && \
+samtools flagstat ${BAM_OUTPUT} > ${OUTPUT_DIR}/${SAMPLE}_flagstat.txt && \
+samtools idxstats ${BAM_OUTPUT} > ${OUTPUT_DIR}/${SAMPLE}_idxstats.txt && \
+samtools depth ${BAM_OUTPUT} > ${OUTPUT_DIR}/${SAMPLE}_coverage.txt
 " 2> ${OUTPUT_DIR}/${SAMPLE}_mapping.log
+
+# Calculate average coverage
+awk '{sum+=$3} END {if (NR > 0) print "Average Coverage: " sum/NR; else print "Average Coverage: 0"}' \
+    ${OUTPUT_DIR}/${SAMPLE}_coverage.txt > ${OUTPUT_DIR}/${SAMPLE}_coverage_summary.txt
 
 echo "Mapping completed for Sample: ${SAMPLE}"
