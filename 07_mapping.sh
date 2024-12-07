98f6fad0e78f2abf4ab0ab86a9671ee9276be7f3
aboss <anna.boss@strudents.unibe.ch>
2024-12-07 17:40:56 +0100

    update of paths

--- 07_mapping.sh
+++ 07_mapping.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=8000
+#SBATCH --mem=32000
 #SBATCH --time=13:00:00
 #SBATCH --array=0-11
 #SBATCH --job-name=mapping
 #SBATCH --mail-user=anna.boss@students.unibe.ch
 #SBATCH --mail-type=fail
-#SBATCH --error=/data/users/aboss/rna_course/error_mapping_%A_%a.e
-#SBATCH --output=/data/users/aboss/rna_course/output_mapping_%A_%a.o
-#SBATCH --partition=pcoursea
-
+#SBATCH --error=./error_mapping_%A_%a.e
+#SBATCH --output=./output_mapping_%A_%a.o
+#SBATCH --partition=pibu_el8
 
 # Directory with input files fastq from cleaned files
-#READS_DIR=/data/users/aboss/rna_course/03_cleaning_results
-READS_DIR=/data/users/aboss/rna_course/03_cleaning_results
+READS_DIR=$(realpath ./analysis/02_cleaning_results)
 # Directory for mapping output    
-OUTPUT_DIR=/data/users/aboss/rna_course/06_mapping_results_stranded
+OUTPUT_DIR=$(realpath ./analysis/04_mapping_results)
 mkdir -p ${OUTPUT_DIR}
 # Directory with reference genome and indexed files
-REFERENCE_GENOME_DIR=/data/users/aboss/rna_course/04_reference_genome
+REFERENCE_GENOME_DIR=$(realpath ./data/02_reference_genome)
 BASENAME='Homo_sapiens.GRCh38_indexed'
-HISAT2_INDEX=/data/users/aboss/rna_course/05_indexing/${BASENAME}
+HISAT2_INDEX=$(realpath ./data/03_indexing/${BASENAME})
 # Samples
 SAMPLES=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
 # Apptainer paths
@@ -37,7 +35,7 @@ READ2="${READS_DIR}/${SAMPLE}_cleaned_R2.fastq.gz"
 # Output SAM and BAM files directly in OUTPUT_DIR
 BAM_OUTPUT="${OUTPUT_DIR}/${SAMPLE}_aligned.sorted.bam"
     
-# Run HISAT2 (for aligning) and samtools (for converting to BAM, sorting, and indexing) using Apptainer
+# Run HISAT2 (for aligning) and samtools (for converting to BAM, sorting, and indexing) using apptainer
 apptainer exec --bind ${REFERENCE_GENOME_DIR} --bind ${READS_DIR} --bind ${OUTPUT_DIR} ${APPTAINER} bash -c "
 hisat2 -x ${HISAT2_INDEX} -1 ${READ1} -2 ${READ2} | \
 samtools view -b - | \
