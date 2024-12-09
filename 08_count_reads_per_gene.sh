85d8cc58c063c772392e3a73dec8324b2963cbe1
aboss <anna.boss@strudents.unibe.ch>
2024-12-09 17:06:02 +0100

    add description, change header adjustment, add flags to feature count

--- 08_count_reads_per_gene.sh
+++ 08_count_reads_per_gene.sh
@@ -13,17 +13,23 @@
 BAM_DIR=$(realpath ./analysis/04_mapping_results)
 ANNOTATION_FILE=$(realpath ./data/02_reference_genome/Homo_sapiens.GRCh38.113.gtf)
 # Output directory and file
-OUTPUT_DIR=$(realpath ./analysis/06_featureCounts_results)
+OUTPUT_DIR=$(realpath ./data/04_featureCounts_results)
 mkdir -p ${OUTPUT_DIR}
 OUTPUT_FILE=${OUTPUT_DIR}/gene_counts.txt
+SUMMARY_FILE=${OUTPUT_FILE}.summary
 # Apptainer paths
 APPTAINER=/containers/apptainer/subread_2.0.6.sif
 
 # List all BAM files in the BAM directory
 BAM_FILES=(${BAM_DIR}/*.bam)
 
-
 # Run featureCounts
+# -g: Specifies the attribute "gene_id" in the annotation file (GTF/GFF) to be used for counting
+# -t: Reads are only counted if they overlap features labeled as exon in the annotation file
+# -Q: Sets a minimum mapping quality score for a read to be included
+# -p: Ensures that paired-end reads are treated as a single unit
+# --countReadPairs: Counts paired-end reads as pairs instead of individual reads
+# --minOverlap 1: Sets the minimum number of overlapping bases between a read and a feature to 1 for the read to be assigned -> For short reads 50 bp) a small value like 1–3 is appropriate
 apptainer exec --bind ${BAM_DIR}:${BAM_DIR} \
                --bind ${OUTPUT_DIR}:${OUTPUT_DIR} \
                --bind $(dirname ${ANNOTATION_FILE}):$(dirname ${ANNOTATION_FILE}) \
@@ -36,30 +42,30 @@ apptainer exec --bind ${BAM_DIR}:${BAM_DIR} \
         -t exon \
         -Q 10 \
         -p \
-        -M 20 \
+        --countReadPairs \
+        --minOverlap 1 \
         "${BAM_FILES[@]}" 2> ${OUTPUT_DIR}/featureCounts_log.log
 
 echo "featureCounts completed. Output saved to ${OUTPUT_FILE}"
 
-# Adjust the header of the output file to include only sample names
+# Adjust the header of the summary file (gene_counts.txt.summary) to include only sample names
 awk 'BEGIN {OFS="\t"} 
-    NR==1 {
-        for (i=2; i<=NF; i++) {
-            split($i, a, "/")
-            split(a[length(a)], b, "_")
+    NR == 1 {
+        for (i = 2; i <= NF; i++) {
+            n = split($i, a, "/")
+            m = split(a[n], b, "_")
             $i = b[1]
         }
     }
-    {print}' gene_counts.txt.summary > gene_counts.txt.summary
+    {print}' ${SUMMARY_FILE} > ${OUTPUT_DIR}/adjusted_gene_counts.txt.summary
 
-# Analyze the output
-SUMMARY_FILE=${OUTPUT_DIR}/gene_counts_summary.txt
-
-# Extract summary information
-awk 'NR > 2 && $1 == "Assigned" {assigned_reads+=$2}
-     NR > 2 && $1 == "Ambiguity" {ambiguous_reads+=$2; ambiguous_count++}
-     END {
-         print "Total Assigned Reads:", assigned_reads
-         print "Total Ambiguous Reads:", ambiguous_reads
-         print "Average Ambiguous Reads Per Sample:", ambiguous_reads/ambiguous_count
-     }' ${OUTPUT_FILE}.summary > ${SUMMARY_FILE}
+# Adjust the header of the counts file (gene_counts.txt) to include only sample names
+awk 'BEGIN {OFS="\t"} 
+    NR == 2 { 
+        for (i = 7; i <= NF; i++) {
+            n = split($i, a, "/")
+            m = split(a[n], b, "_")
+            $i = b[1]
+        }
+    }
+    {print}' ${OUTPUT_FILE} > ${OUTPUT_DIR}/adjusted_gene_counts.txt
